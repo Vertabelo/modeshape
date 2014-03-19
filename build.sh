9195a449bb33ea5458fe6e4abbfa44b062734513
Michał Kołodziejski <mkolodziejski@e-point.pl>
2014-03-19 12:51:37 +0100

    Przydatne skrypty

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export MAVEN_HOME=~/bin/apache-maven-3.2.1/bin/; export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"; ~/bin/apache-maven-3.2.1/bin/mvn clean install -s settings.xml
---
+++ update_modeshape_jars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.2.Final-ep/modeshape-sequencer-ddl-3.7.2.Final-ep.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.2.Final-ep/modeshape-sequencer-ddl-3.7.2.Final-ep-sources.jar ~/.maven/repository/modeshape/src/modeshape-sequencer-ddl-3.7.2.Final-ep.zip
+
+cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.2.Final-ep/modeshape-common-3.7.2.Final-ep.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.2.Final-ep/modeshape-common-3.7.2.Final-ep-sources.jar ~/.maven/repository/modeshape/src/modeshape-common-3.7.2.Final-ep.zip
+
+cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.2.Final-ep/modeshape-jcr-api-3.7.2.Final-ep.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.2.Final-ep/modeshape-jcr-api-3.7.2.Final-ep-sources.jar ~/.maven/repository/modeshape/src/modeshape-jcr-api-3.7.2.Final-ep.zip
+
