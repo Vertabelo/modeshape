877b88221e5e58b820ab7eb3d00ac68128ba6a79
Michał Kołodziejski <mkolodziejski@e-point.pl>
2014-05-27 15:01:02 +0200

    Nowa wersja developerska - 3.7.1.Final-ep3

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep</version>|<version>3.7.1.Final-ep2</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep2</version>|<version>3.7.1.Final-ep3</version>|g'
