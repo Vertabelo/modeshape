e86fcaea9b1fac3d938c0c554216b1316f4a3aaa
Michał Kołodziejski <mkolodziejski@e-point.pl>
2014-05-27 13:30:50 +0200

    Aktualizacja skryptu deployującego

--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep1.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep1-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-ep1.zip
+scp modeshape-common/target/modeshape-common-3.7.1.Final-ep2.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-common/target/modeshape-common-3.7.1.Final-ep2-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-ep2.zip
 
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep1.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep1-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep1.zip
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep2.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep2-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep2.zip
 
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep1.jar alfa:/var/www/maven/modeshape/jars/
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep1-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep1.zip
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep2.jar alfa:/var/www/maven/modeshape/jars/
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep2-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep2.zip
 
 
