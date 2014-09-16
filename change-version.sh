77b0be2ab7efb1ea79f8c7b666a6eefc45556e89
Agnieszka Kozubek <akozubek@e-point.pl>
2014-09-16 09:10:35 +0200

    Nowa wersja deweloperska

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep8</version>|<version>3.7.1.Final-ep8</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep9-dev</version>|<version>3.7.1.Final-ep9-dev</version>|g'
--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep8.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep8-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-ep8.zip
+scp modeshape-common/target/modeshape-common-3.7.1.Final-ep9-dev.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-common/target/modeshape-common-3.7.1.Final-ep9-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-ep9-dev.zip
 
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep8.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep8-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep8.zip
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep9-dev.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep9-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep9-dev.zip
 
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep8.jar alfa:/var/www/maven/modeshape/jars/
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep8-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep8.zip
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep9-dev.jar alfa:/var/www/maven/modeshape/jars/
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep9-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep9-dev.zip
 
 
