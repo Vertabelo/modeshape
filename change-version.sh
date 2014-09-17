b27f40ac4377f577dc4f932dfe6516ac3db87582
Michał Kołodziejski <mkolodziejski@e-point.pl>
2014-09-17 16:44:51 +0200

    Nowa wersja developerska

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep9</version>|<version>3.7.1.Final-ep9</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep10-dev</version>|<version>3.7.1.Final-ep10-dev</version>|g'
