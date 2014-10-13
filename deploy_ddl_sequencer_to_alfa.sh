89761618c761791d0f9f2a443c1bc50a996789f3
Rafal Strzalinski <nabla@e-point.pl>
2014-10-13 13:41:33 +0200

    Wersja 3.7.1.Final-ep10

--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep10-dev.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-common/target/modeshape-common-3.7.1.Final-ep10-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-ep10-dev.zip
+version=ep10
 
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep10-dev.jar alfa:/var/www/maven/modeshape/jars/
-scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-ep10-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep10-dev.zip
+scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-${version}.zip
 
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev.jar alfa:/var/www/maven/modeshape/jars/
-scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev.zip
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
+scp modeshape-jcr-api/target/modeshape-jcr-api-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-jcr-api-3.7.1.Final-${version}.zip
+
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
+scp sequencers/modeshape-sequencer-ddl/target/modeshape-sequencer-ddl-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-${version}.zip
 
 
--- update_modeshape_jars.sh
+++ update_modeshape_jars.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.1.Final-ep10-dev/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev.jar ~/.maven/repository/modeshape/jars/
-cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.1.Final-ep10-dev/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev-sources.jar ~/.maven/repository/modeshape/src/modeshape-sequencer-ddl-3.7.1.Final-ep10-dev.zip
 
-cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.1.Final-ep10-dev/modeshape-common-3.7.1.Final-ep10-dev.jar ~/.maven/repository/modeshape/jars/
-cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.1.Final-ep10-dev/modeshape-common-3.7.1.Final-ep10-dev-sources.jar ~/.maven/repository/modeshape/src/modeshape-common-3.7.1.Final-ep10-dev.zip
+cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.1.Final-ep*/modeshape-sequencer-ddl-3.7.1.Final-ep*.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-sequencer-ddl/3.7.1.Final-ep*/modeshape-sequencer-ddl-3.7.1.Final-ep*-sources.jar ~/.maven/repository/modeshape/src/
 
-cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.1.Final-ep10-dev/modeshape-jcr-api-3.7.1.Final-ep10-dev.jar ~/.maven/repository/modeshape/jars/
-cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.1.Final-ep10-dev/modeshape-jcr-api-3.7.1.Final-ep10-dev-sources.jar ~/.maven/repository/modeshape/src/modeshape-jcr-api-3.7.1.Final-ep10-dev.zip
+cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.1.Final-ep*/modeshape-common-3.7.1.Final-ep*.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-common/3.7.1.Final-ep*/modeshape-common-3.7.1.Final-ep*-sources.jar ~/.maven/repository/modeshape/src/
+
+cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.1.Final-ep*/modeshape-jcr-api-3.7.1.Final-ep*.jar ~/.maven/repository/modeshape/jars/
+cp ~/.m2/repository/org/modeshape/modeshape-jcr-api/3.7.1.Final-ep*/modeshape-jcr-api-3.7.1.Final-ep*-sources.jar ~/.maven/repository/modeshape/src/
 
