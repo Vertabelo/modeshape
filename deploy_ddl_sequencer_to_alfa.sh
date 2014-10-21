1d85c8264b3d15cc9514ef58115aeba0e37d7577
Michał Kołodziejski <mkolodziejski@e-point.pl>
2014-10-21 10:44:59 +0200

    Zmiana nr wersji w deployowaczce

--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=ep10
+version=ep11-dev
 
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-${version}.zip
