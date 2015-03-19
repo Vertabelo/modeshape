4bfa8927b70f0ab851548deac01b8c8d19a8d0eb
Agnieszka Kozubek-Krycuń <akozubek@e-point.pl>
2015-03-19 11:03:00 +0100

    Nowa wersja deweloperska

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep15</version>|<version>3.7.1.Final-ep15</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep16-dev</version>|<version>3.7.1.Final-ep16-dev</version>|g'
