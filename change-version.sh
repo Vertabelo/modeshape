c71d0e32cc4b80b34768c696455e92afdf32c6e9
Michał Kołodziejski <mkolodziejski@e-point.pl>
2016-05-11 15:10:37 +0200

    Wersja 3.7.1.Final-ep18

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep18-dev</version>|<version>3.7.1.Final-ep18-dev</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep18</version>|<version>3.7.1.Final-ep18</version>|g'
