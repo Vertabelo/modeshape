ae6ad41ec57e4f1a98729c57ad4a473b025f7a0f
Michał Kołodziejski <mkolodziejski@e-point.pl>
2016-05-11 16:07:56 +0200

    Nowa wersja developerska

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep18</version>|<version>3.7.1.Final-ep18</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep19-dev</version>|<version>3.7.1.Final-ep19-dev</version>|g'
--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=ep18-dev
+version=ep19-dev
 
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-${version}.zip
