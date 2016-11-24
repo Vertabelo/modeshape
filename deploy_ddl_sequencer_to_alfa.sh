1834d482501423db2711f5fa31139fa8876cba01
Adam Mościcki <amoscicki@e-point.pl>
2016-11-24 14:48:48 +0100

    Wersja 3.7.1.Final-ep19
    Change-Id: I92dd0997e792dd7c61d951b356200b00f609b7aa

--- deploy_ddl_sequencer_to_alfa.sh
+++ deploy_ddl_sequencer_to_alfa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=ep19-dev
+version=ep19
 
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}.jar alfa:/var/www/maven/modeshape/jars/
 scp modeshape-common/target/modeshape-common-3.7.1.Final-${version}-sources.jar alfa:/var/www/maven/modeshape/src/modeshape-common-3.7.1.Final-${version}.zip
