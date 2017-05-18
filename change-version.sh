9586ec5ef5ee78a2c9ebec3fcbb53cbde15de953
Adam Mościcki <amoscicki@e-point.pl>
2017-05-18 10:46:21 +0200

    Wersja 3.7.1.Final-ep20
    Change-Id: Ibee87bc3b834bf589c747be5e648ed1650c9a571

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep20-dev</version>|<version>3.7.1.Final-ep20-dev</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep20</version>|<version>3.7.1.Final-ep20</version>|g'
