e3538e7a2e97120130b8e4f52c3759fbbb218af0
Adam Mościcki <amoscicki@e-point.pl>
2018-01-18 14:50:22 +0100

    Nowa wersja developerska
    Change-Id: I7ad9d2b85b2aa01aa3c767ffdd3818480271b890

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep21</version>|<version>3.7.1.Final-ep21</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep22-dev</version>|<version>3.7.1.Final-ep22-dev</version>|g'
