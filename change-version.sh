24c5f14d1291a6c3e9e7b406df03f56491acce0f
Adam Mościcki <amoscicki@e-point.pl>
2019-06-25 17:37:34 +0200

    Wersja developersja ep23-dev
    Change-Id: Iaf10db2bca838346d6fbade6decaaa9f09370854

--- change-version.sh
+++ change-version.sh
@@ -1,1 +1,1 @@
-find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep22</version>|<version>3.7.1.Final-ep22</version>|g'
+find . -type f -print0 | xargs -0 sed -i 's|<version>3.7.1.Final-ep23-dev</version>|<version>3.7.1.Final-ep23-dev</version>|g'
